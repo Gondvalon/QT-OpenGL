6805ce0ed93bc0a823376902c58790dccff202a7
PhilippJahr <philippjahr@hotmail.de>
2021-08-01 01:45:57 +0200

    OpenGL Project

---
+++ OpenGL/build/CMakeFiles/OpenGL.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Cplusplus_lernen/OpenGL/main.cpp" "D:/Cplusplus_lernen/OpenGL/build/CMakeFiles/OpenGL.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../glfw-3.3.4/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "D:/Cplusplus_lernen/OpenGL/build/glfw-3.3.4/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpenGL/build/CMakeFiles/OpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL.dir/main.cpp.obj"
+  "OpenGL.exe"
+  "OpenGL.exe.manifest"
+  "OpenGL.lib"
+  "OpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
